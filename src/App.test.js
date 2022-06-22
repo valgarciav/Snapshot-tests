86e918cb40ab6f73e8a7c7e4ca4b7162f66a65ff
valgarciav <vkgarcia@gmail.com>
2022-06-21 21:58:27 -0400

    first commit
    snapshot tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,6 @@
 import { render, screen } from "@testing-library/react";
-import renderer from "react-test-renderer";
 import App from "./App";
+import renderer from "react-test-renderer";
 
 test("renders a snapshot", () => {
   const tree = renderer.create(<App />).toJSON();
